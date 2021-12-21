0543980812634756b9d8d7ba8d561c649acfadb9
CalixtoElProgramador <calixto.programador@gmail.com>
2021-12-20 22:55:40 -0600

    style: The UI to select a profile picture was created.

---
+++ app/src/main/java/com/listocalixto/android/ecommerce/activities/SaveImageActivity.kt
@@ -0,0 +1,12 @@
+package com.listocalixto.android.ecommerce.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.listocalixto.android.ecommerce.R
+
+class SaveImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_image)
+    }
+}
